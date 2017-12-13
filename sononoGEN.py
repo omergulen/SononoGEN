fb9810fd76188d8f7d40b7d99ab865e040b3ede6
Ömer <omergulen@std.iyte.edu.tr>
2017-12-13 15:36:59 +0300

    Create sononoGEN.py

---
+++ sononoGEN.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""Simple Bot to send timed Telegram messages.
+
+# This program is dedicated to the public domain under the CC0 license.
+
+This Bot uses the Updater class to handle the bot and the JobQueue to send
+timed messages.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Alarm Bot example, sends a message after a set time.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+from telegram.ext import Updater, CommandHandler
+import logging
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    update.message.reply_text('Hi! /t and letter to troll. (eg. /t o sanane)\n'
+                              'or you can just reply to message with /t and letter (/t o)')
+
+def about(bot, update):
+    update.message.reply_text('To advice us:\n'
+                      'https://github.com/omergulen/telegram-currency-bot\n'
+                      'or\n'
+                      'omrglen@gmail.com\n'
+                      'Thanks for support!')
+
+
+def alarm(bot, job):
+    """Send the alarm message."""
+    bot.send_message(job.context, text='Beep!')
+
+
+def translator(bot, update, args):
+    vovals = "aeıiuüoöAEIİUÜOÖ"
+    voval = args[0] * 8 + args[0].upper() * 8
+    text = " ".join(args[1:])
+    translation = str.maketrans(vovals, voval)
+
+    try:
+        update.message.reply_text(update.message.reply_to_message['text'].translate(translation))
+    except:
+        update.message.reply_text(text.translate(translation))
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    """Run bot."""
+    updater = Updater("************************************************")
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("about", about))
+    dp.add_handler(CommandHandler("t", translator,
+                                  pass_args=True))
+    #dp.add_handler(CommandHandler("unset", unset, pass_chat_data=True))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
+    # SIGABRT. This should be used most of the time, since start_polling() is
+    # non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()

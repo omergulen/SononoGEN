597af646f72eed8a47b09fb850fb43b0370fd303
Ömer <omergulen@std.iyte.edu.tr>
2017-12-13 17:08:23 +0300

    Update sononoGEN.py

--- sononoGEN.py
+++ sononoGEN.py
@@ -1,24 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-"""Simple Bot to send timed Telegram messages.
-
-# This program is dedicated to the public domain under the CC0 license.
-
-This Bot uses the Updater class to handle the bot and the JobQueue to send
-timed messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Alarm Bot example, sends a message after a set time.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 from telegram.ext import Updater, CommandHandler
 import logging
 
@@ -79,9 +61,6 @@ def main():
     # Start the Bot
     updater.start_polling()
 
-    # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
-    # SIGABRT. This should be used most of the time, since start_polling() is
-    # non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
